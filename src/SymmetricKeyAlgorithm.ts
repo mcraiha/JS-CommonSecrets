28ae40df476468f89ee907a63a9d243bcc04dd45
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-01-27 16:22:56 +0200

    First WIP of SymmetricKeyAlgorithm and AES-CTR
    Still very WIP, includes one test case

---
+++ src/SymmetricKeyAlgorithm.ts
@@ -0,0 +1,228 @@
+import { BitConverter } from "./BitConverter.ts";
+import { ChecksumHelper } from "./ChecksumHelper.ts";
+
+export enum SymmetricEncryptionAlgorithm
+{
+  AES_CTR = 0,
+  ChaCha20
+}
+
+export class SymmetricKeyAlgorithm
+{
+  /** Symmetric Encryption Algorithm as string */
+  public algorithm: string;
+
+  /** Key size in bits */
+  public keySizeInBits: number;
+
+  public settingsAES_CTR: SettingsAES_CTR | null = null;
+
+  public settingsChaCha20: SettingsChaCha20 | null = null;
+
+  public static allowed_AES_CTR_KeyLengths: number[] = [ 16, 24, 32 ];
+
+  /**
+   * Default constructor for Symmetric key algorithm
+   * @param algorithm Algorithm to use
+   * @param keySizeInBits Key size in bits, e.g. 256
+   * @param settings Settings for chosen algorithm
+   */
+  constructor(algorithm: SymmetricEncryptionAlgorithm, keySizeInBits: number, settings: SettingsAES_CTR | SettingsChaCha20)
+  {
+    this.algorithm = algorithm.toString();
+
+    if (algorithm == SymmetricEncryptionAlgorithm.AES_CTR)
+    {
+      if (!SymmetricKeyAlgorithm.allowed_AES_CTR_KeyLengths.includes(keySizeInBits / 8))
+      {
+        throw Error(`${keySizeInBits} is not valid AES-CTR key size!`);
+      }
+
+      this.settingsAES_CTR = <SettingsAES_CTR>settings!;
+    }
+    else if (algorithm == SymmetricEncryptionAlgorithm.ChaCha20)
+    {
+      if (256 != keySizeInBits)
+      {
+        throw Error(`${keySizeInBits} is not valid ChaCha20 key size!`);
+      }
+
+      this.settingsChaCha20 = <SettingsChaCha20>settings!;
+    }
+    else
+    {
+      throw Error(`${algorithm} constructor not implemented yet!`);
+    }
+
+    this.keySizeInBits = keySizeInBits;
+  }
+
+  /**
+   * Encrypt given bytes with given key. Returns new array with encrypted bytes
+   * @param bytesToEncrypt Byte array to encrypt
+   * @param key Key
+   * @returns Encrypted bytes in new array
+   */
+  public async EncryptBytes(bytesToEncrypt: Uint8Array, key: Uint8Array): Promise<Uint8Array>
+  {
+    let returnArray: Uint8Array = new Uint8Array(bytesToEncrypt.length);
+
+    if (this.algorithm === SymmetricEncryptionAlgorithm.AES_CTR.toString())
+    {
+      const cryptoKey = await crypto.subtle.importKey(
+        "raw",
+        key,
+        "AES-CTR",
+        false,
+        ["encrypt", "decrypt"]
+      );
+      const encryptedArray = await crypto.subtle.encrypt(
+        {
+          name: "AES-CTR",
+          counter: this.settingsAES_CTR!.initialCounter,
+          length: this.keySizeInBits
+        },
+        cryptoKey,
+        bytesToEncrypt);
+      returnArray = new Uint8Array(encryptedArray);
+    }
+    else if (this.algorithm === SymmetricEncryptionAlgorithm.ChaCha20.toString())
+    {
+      
+    }
+    else
+    {
+      throw Error("Not implemented!");
+    }
+
+    return returnArray;
+  }
+
+  /**
+   * Decrypt given bytes with given key. Returns new array with decrypted bytes. Same result as EncryptBytes
+   * @param bytesToDecrypt Byte array to decrypt
+   * @param key Key
+   * @returns Decrypted bytes in new array
+   */
+  public async DecryptBytes(bytesToDecrypt: Uint8Array, key: Uint8Array): Promise<Uint8Array>
+  {
+    return await this.EncryptBytes(bytesToDecrypt, key);
+  }
+}
+
+export class SettingsAES_CTR
+{
+  /** Initial counter as byte array */
+  public initialCounter: Uint8Array;
+
+  /**
+   * Default constructor for SettingsAES_CTR
+   * @param initialCounter Byte array of initial counter
+   */
+  constructor(initialCounter: Uint8Array)
+  {
+    if (initialCounter == null)
+    {
+      throw Error("Initial counter cannot be null!");
+    }
+    else if (initialCounter.length != 16)
+    {
+      throw Error("Initial counter only allows length of 16 bytes!");
+    }
+
+    this.initialCounter = initialCounter;
+  }
+
+  /**
+   * Get Settings as bytes
+   * @returns Byte array
+   */
+  public GetSettingsAsBytes(): Uint8Array
+  {
+    return this.initialCounter;
+  }
+
+  /**
+   * Create With Cryptographic Random Numbers
+   */
+  public static CreateWithCryptographicRandomNumbers(): SettingsAES_CTR
+  {
+    const initialCounter: Uint8Array = new Uint8Array(16);
+    crypto.getRandomValues(initialCounter);
+    return new SettingsAES_CTR(initialCounter);
+  }
+}
+
+export class SettingsChaCha20
+{
+  /** Nonce byte array */
+  public nonce: Uint8Array;
+
+  /** Counter */
+  public counter: number;
+
+  /**
+   * Default constructor for SettingsChaCha20
+   * @param nonce Nonce as byte array
+   * @param counter Counter
+   */
+  constructor(nonce: Uint8Array, counter: number)
+  {
+    if (nonce == null)
+    {
+      throw Error("Nonce cannot be null!");
+    }
+    else if (nonce.length != 12)
+    {
+      throw Error("Nonce only allows length of 12 bytes!");
+    }
+
+    this.nonce = nonce;
+    this.counter = counter;
+  }
+
+  /**
+   * Increase nonce
+   */
+  public IncreaseNonce(): void
+  {
+    let index: number = 0;
+		let done: boolean = false;
+    while (!done && index < this.nonce.length)
+    {
+      if (this.nonce[index] < 255)
+      {
+        this.nonce[index]++;
+        done = true;
+      }
+      else
+      {
+        this.nonce[index] = 0;
+      }
+      index++;
+    }
+  }
+
+  /**
+   * Get settings as byte array
+   */
+  public GetSettingsAsBytes(): Uint8Array
+  {
+    return ChecksumHelper.JoinByteArrays(new Array(this.nonce, BitConverter.GetBytes(this.counter)));
+  }
+
+  /**
+   * Create SettingsChaCha20 with Cryptographic random numbers, you should use this instead of constructor
+   * @returns SettingsChaCha20
+   */
+  public static CreateWithCryptographicRandomNumbers(): SettingsChaCha20
+  {
+    const nonce: Uint8Array = new Uint8Array(8);
+    const uintBytes: Uint8Array = new Uint8Array(4);
+
+    crypto.getRandomValues(nonce);
+    crypto.getRandomValues(uintBytes);
+
+    return new SettingsChaCha20(new Uint8Array([...nonce, ...new Uint8Array(4)]), new DataView(uintBytes.buffer).getUint32(0, true)); 
+  }
+}
---
+++ tests/SymmetricKeyAlgorithm_test.ts
@@ -0,0 +1,25 @@
+import { assert, assertEquals, assertExists, assertRejects } from "https://deno.land/std@0.122.0/testing/asserts.ts";
+import { KeyDerivationFunctionEntry, KeyDerivationPrf } from "../src/KeyDerivationFunctionEntry.ts";
+import { SymmetricEncryptionAlgorithm, SymmetricKeyAlgorithm, SettingsAES_CTR, SettingsChaCha20 } from "../src/SymmetricKeyAlgorithm.ts";
+import { CalculateEntropy } from "./ComparisonHelper.ts";
+
+Deno.test("AES_CTR test", async () => {
+  // Arrange
+  const key: Uint8Array = new Uint8Array([0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c]);
+  const initialCounter: Uint8Array = new Uint8Array([0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff]);
+
+  const settingsAES_CTR: SettingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+  const skaAES_CTR: SymmetricKeyAlgorithm = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+  const content: Uint8Array = new Uint8Array([0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a]);
+  const expected: Uint8Array = new Uint8Array([0x87, 0x4d, 0x61, 0x91, 0xb6, 0x20, 0xe3, 0x26, 0x1b, 0xef, 0x68, 0x64, 0x99, 0x0d, 0xb6, 0xce]);
+
+  // Act
+  const output1 = await skaAES_CTR.EncryptBytes(content, key);
+  const output2 = await skaAES_CTR.DecryptBytes(content, key);
+
+  // Assert
+  assertEquals(output1, expected);
+  assertEquals(output2, expected);
+});
